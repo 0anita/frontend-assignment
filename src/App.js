718478650b75b92e0c7a1658bfcf35259171875a
0anita <maharjanoneta237@gmail.com>
2023-07-04 23:23:22 +0545

    homepage-showallproductsdone header and catergory left

--- src/App.js
+++ src/App.js
@@ -7,7 +7,35 @@ const {isLoading, isError, error, data} = useGetAllProductsQuery()
 console.log(data)
   return (
     <div>
-      
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 p-10 gap-5'>
+        {
+          data && data.map((itemNumber) => {
+            return <div 
+            className=' border border-gray-300 hover:shadow-lg p-5'
+            key={itemNumber.id}>
+              <div className='grid grid-row-3'>
+              <div className='flex justify-center'>
+                <img src={itemNumber.image} alt="" 
+              className='h-28 w-auto object-contain'/>
+              </div>
+          
+              <div className='h-16 py-4'>
+            <p className='line-clamp-2'>
+              {itemNumber.title}
+            </p>
+            </div>
+            <div className='py-3'>
+            <p className='text-xl text-cyan-900'>
+              ${itemNumber.price}
+            </p>
+            <p className='text-sm text-gray-500'>Rating: {itemNumber.rating.rate}({itemNumber.rating.count})</p>
+            </div>
+            </div>
+            </div>
+              
+          })
+        }
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      '2xl': { 'max': '1536px' },
+      'xl': { 'max': '1280px' },
+      'lg': { 'max': '1024px' },
+      'md': { 'max': '768px' },
+      'sm': { 'max': '640px' },
+    },
   },
   plugins: [],
 }
-
